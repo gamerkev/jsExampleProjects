69bf246795fda1492a9c2d0ab3cc7ee6f408b5eb
gamerkev <gooner@gooning.com>
2025-08-15 20:16:05 +0100

    addded arrays folder and shoppingList program

---
+++ arrays/shoppingList.js
@@ -0,0 +1,24 @@
+let arr = ["apples", "pears", "bananas"];
+console.log(`currently we have to buy ${arr}`);
+let item = "mango";
+arr.push(item);
+console.log(`after adding ${item}, our array now looks like ${arr}`);
+arr.pop();
+console.log(`we've found and purchased mango! so we've updated our shopping list to reflect this change, ${arr}`);
+
+let shoppingList = [arr, ["chicken breast", "minced meat"]];
+
+function countItems(nestedArr){
+    let count = 0;
+    for(i = 0; i<nestedArr.length; i=i+1){
+        count += nestedArr[i].length;
+    }
+    return count;
+}
+
+console.log(`we also need proteins, so we've created a shopping list with fruits and proteins separated`);
+console.log(`the whole list is ${shoppingList} and the fruits are ${shoppingList[0]}, with the proteins being ${shoppingList[1]}`);
+console.log(`we need ${countItems(shoppingList)} items, ${shoppingList[0].length} of them being fruits, and ${shoppingList[1].length} being proteins`);
+
+shoppingList[1][shoppingList[1].indexOf("minced meat")] = "quorn";
+console.log(`the in-laws are coming over, better get quorn rather than mince, our new shopping list is ${shoppingList}`);
